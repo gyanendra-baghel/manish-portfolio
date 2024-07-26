c2cf085ad0212bcc3e961d3d059f1dd01a83a19f
Gyanendra Baghel <gyanendrabaghel633@gmail.com>
2024-07-26 08:43:29 +0530

    remove ' build  bug

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -27,10 +27,10 @@ export function HeroSection() {
                 }}
                 className="m-0 p-0 "
               > */}
-                <img
-                  src="https://res.cloudinary.com/dps3eybtx/image/upload/v1721406425/Portfolio/photo/kxh82tlkxlthcn4wnrez.png"
-                  className="mt-16 bg-transparent h-52 w-52 rounded-full "
-                ></img>
+              <img
+                src="https://res.cloudinary.com/dps3eybtx/image/upload/v1721406425/Portfolio/photo/kxh82tlkxlthcn4wnrez.png"
+                className="mt-16 bg-transparent h-52 w-52 rounded-full "
+              ></img>
               {/* </motion.h1> */}
             </Lampsm>
           </div>
@@ -48,18 +48,21 @@ export function HeroSection() {
           />
           <div className=" p-1 max-w-7xl  pl-3 relative z-10  w-full pt-1 md:pt-0">
             <h1 className="text-3xl md:text-7xl md:max-[250]:text-5xl font-bold text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-              Hi,<br></br>I'm Manish Patidar,
+              Hi,<br></br>I&apos;m Manish Patidar,
             </h1>
             <p className="my-2 font-normal md:text-lg text-base text-neutral-100  min-w-9">
               a passionate full-stack developer specializing in the MERN
               (MongoDB, Express, React, Node.js) stack. With a strong foundation
               in both front-end and back-end development, I excel at building
               scalable, efficient, and user-friendly applications.My journey in
-              programming began 2 Years ago, and since then, I've been
-              constantly learning and growing as a developer. I've worked on
-              various projects.
+              programming began 2 Years ago, and since then, I&apos;ve been
+              constantly learning and growing as a developer. I&apos;ve worked
+              on various projects.
             </p>
-            <a href="https://drive.google.com/file/d/1RReJobvASubJ93jON8Io3GxnOCEnpTvp/view?usp=sharing" target="_blank">
+            <a
+              href="https://drive.google.com/file/d/1RReJobvASubJ93jON8Io3GxnOCEnpTvp/view?usp=sharing"
+              target="_blank"
+            >
               <button
                 className=" relative inline-flex items-center 
               justify-center p-0.5 mb-2 me-2 overflow-hidden text-xl font-medium text-black rounded-full group bg-gradient-to-br from-cyan-600 to-cyan-400 group-hover:from-cyan-600
@@ -90,29 +93,33 @@ export function HeroSection() {
         />
         <div className=" p-4 max-w-7xl  pl:3 xl:pl-20 2xl:pl-40 relative z-10  w-6/12 pt-20 md:pt-0">
           <h1 className="text-5xl xl:text-6xl  2xl:text-7xl font-bold text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-            Hi,<br></br>I'm Manish Patidar,
+            Hi,<br></br>I&apos;m Manish Patidar,
           </h1>
           <p className="my-4 font-normal text-lg text-neutral-100  min-w-9">
             a passionate full-stack developer specializing in the MERN (MongoDB,
             Express, React, Node.js) stack. With a strong foundation in both
             front-end and back-end development, I excel at building scalable,
             efficient, and user-friendly applications.My journey in programming
-            began 2 Years ago, and since then, I've been constantly learning and
-            growing as a developer. I've worked on various projects.
+            began 2 Years ago, and since then, I&apos;ve been constantly
+            learning and growing as a developer. I&apos;ve worked on various
+            projects.
           </p>
-          <a href="https://drive.google.com/file/d/1RReJobvASubJ93jON8Io3GxnOCEnpTvp/view?usp=sharing" target="_blank">
-          <button
-                className=" relative inline-flex items-center 
+          <a
+            href="https://drive.google.com/file/d/1RReJobvASubJ93jON8Io3GxnOCEnpTvp/view?usp=sharing"
+            target="_blank"
+          >
+            <button
+              className=" relative inline-flex items-center 
               justify-center p-0.5 mb-2 me-2 overflow-hidden text-xl font-medium text-black rounded-full group bg-gradient-to-br from-cyan-600 to-cyan-400 group-hover:from-cyan-600
                group-hover:to-cyan-400 hover:text-black dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-500 dark:focus:ring-cyan-800"
-              >
-                <span
-                  className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white
+            >
+              <span
+                className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white
                  dark:bg-slate-950 rounded-full group-hover:bg-opacity-0"
-                >
-                  Download Now
-                </span>
-              </button>
+              >
+                Download Now
+              </span>
+            </button>
           </a>
         </div>
 
@@ -127,12 +134,11 @@ export function HeroSection() {
                 duration: 0.8,
                 ease: "easeInOut",
               }}
-             
             >
-            <img
-              src="https://res.cloudinary.com/dps3eybtx/image/upload/v1721406425/Portfolio/photo/kxh82tlkxlthcn4wnrez.png"
-              className="bg-transparent h-52 w-52 rounded-full "
-            ></img>
+              <img
+                src="https://res.cloudinary.com/dps3eybtx/image/upload/v1721406425/Portfolio/photo/kxh82tlkxlthcn4wnrez.png"
+                className="bg-transparent h-52 w-52 rounded-full "
+              ></img>
             </motion.h1>
           </LampContainer>
         </div>
@@ -145,4 +151,4 @@ export function HeroSection() {
   );
 }
 
-// 
+//
